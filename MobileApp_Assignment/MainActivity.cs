a446b2ec2f73dd859a2c46f7aa2661389ae86d62
Eric Scott <Eric-_-Scott@hotmail.com>
2018-03-21 15:29:56 +0000

    Add project files.

---
+++ MobileApp_Assignment/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace MobileApp_Assignment
+{
+    [Activity(Label = "MobileApp_Assignment", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
