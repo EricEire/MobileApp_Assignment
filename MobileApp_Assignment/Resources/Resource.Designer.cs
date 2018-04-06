df7ead43762faad01d19ea4c6c689dc611cd6bb4
Eric Scott <Eric-_-Scott@hotmail.com>
2018-04-06 10:36:16 +0100

    buttons styled

--- MobileApp_Assignment/Resources/Resource.Designer.cs
+++ MobileApp_Assignment/Resources/Resource.Designer.cs
@@ -67,22 +67,31 @@ namespace MobileApp_Assignment
 			public const int Brownie = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int btn = 2130837506;
+			public const int btnhome = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int Cupcake1 = 2130837507;
+			public const int button = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int recipeBg = 2130837508;
+			public const int buttonbg1 = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int Recipebg2 = 2130837509;
+			public const int buttonbwrds = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int TempBg = 2130837510;
+			public const int Cupcake1 = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int Weightbg = 2130837511;
+			public const int recipeBg = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int Recipebg2 = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int TempBg = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int Weightbg = 2130837514;
 			
 			static Drawable()
 			{
